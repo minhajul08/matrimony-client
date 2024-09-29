a7f2a98fd438ac08adbfda314df132ad5a132b77
minhajul08 <minhazmehadi61@gmail.com>
2024-09-29 09:41:23 +0600

    added footer

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <p>Ok</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+        dfgs
+        </div>
+    );
+};
+
+export default Home;
